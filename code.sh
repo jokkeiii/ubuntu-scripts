4c2fe5518138073cac400a7124ff7ae5e96ecf5c
jokkeiii <54310059+jokkeiii@users.noreply.github.com>
2024-04-30 19:11:02 +0300

    added parenthesis to url

--- code.sh
+++ code.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget --output-document=code.deb -P ~/Downloads https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+wget --output-document=code.deb -P ~/Downloads "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 
 sudo apt install ~/Downloads/code.deb -y
